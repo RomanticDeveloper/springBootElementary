bff9ffe1b59bba920db076b8413e73424328d269
RomanticDeveloper <seokk1209@gmail.com>
2021-11-27 08:24:28 +0900

    2021.11.27 getApi Finishied

--- README.md
+++ README.md
@@ -5,3 +5,12 @@
 > 작성자 : Ro.D
 
 
+
+1. Query Param 을 묵시적으로 받는 경우 , "Map  < String, String >" 을 통해 받을 수 있고, 명시적으로 받는 경우에는 @RequestParma 어노테이션을 활용해서 @RequestParam name 등으로 받아 올 수 있음
+
+2. Dto 에서 객체를 만들어서 받아오는 경우에는 @RequestParam 을 사용하지 않아도 객체를 받아올 수 있다.
+1) UserReqest 객체 생성후 queryParam(UserRequest userRequest) 이와같은 형태로 userRequest 를 만들 수 있다.
+
+
+
+
--- src/main/java/com/example/hello/controller/ApiController.java
+++ src/main/java/com/example/hello/controller/ApiController.java
@@ -1,5 +1,6 @@
 package com.example.hello.controller;
 
+import com.example.hello.dto.UserRequest;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -27,16 +28,46 @@ public class ApiController {
         System.out.println("PathVaribale : " + pahtName);
         return "";
     }
-//
-//    @GetMapping(path = "query-param")
-//    public String qeryParame(Map<String, String> queryParam){
-//        queryParam.entrySet().forEach(entry ->{
-//
-//                }
-//
-//        );
-//    }
+
+    @GetMapping(path = "query-param")
+    public String qeryParame(@RequestParam Map<String, String> queryParam){
+
+        StringBuilder sb = new StringBuilder();
+        queryParam.entrySet().forEach(entry ->{
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+            System.out.println("");
+
+            sb.append(entry.getKey() + "=" + entry.getValue() +"\n");
+        });
+        return sb.toString();
+    }
 
     // http://localhost:9090/api/get/query-params?user=steve&email=steve@gmail.com&age=30
+
+    @GetMapping("query-param02")
+    public String queryParam02(@RequestParam String name,
+                               @RequestParam String email,
+                               @RequestParam int age){
+
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(age);
+
+        return name + " " + email + " " + age;
+    }
+
+    @GetMapping("query-param03")
+    public String queryParam03(UserRequest userRequest){
+
+        System.out.println(userRequest.getName());
+        System.out.println(userRequest.getEmail());
+        System.out.println(userRequest.getAge());
+
+        return userRequest.toString();
+    }
+
+
+
 
 }
